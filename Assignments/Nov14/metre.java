f28911e312e32927c7739ecab840d23ae2e0ac84
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-11-14 23:45:24 +0530

    Add files via upload

---
+++ Assignments/Nov14/metre.java
@@ -0,0 +1,17 @@
+package geek1;
+
+import java.util.Scanner;
+
+public class metre {
+	public static void main(String[] args)
+	{
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		double i,m;
+		System.out.println("Enter the length in inches:");
+		i=sc.nextDouble();
+	    m=i*0.0254;
+		System.out.println(m+"m");
+	}
+
+}

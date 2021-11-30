a9c7775bea6f647c9739496f7af247a219b7b066
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-11-30 21:25:39 +0530

    Update vehicle.java

--- Assignments/Nov29/vehicle.java
+++ Assignments/Nov29/vehicle.java
@@ -13,19 +13,19 @@ public class Automobiles
 		System.out.println("^^^^ CAR ^^^^");
 		Vehicle car  = new Vehicle();//Constructor is always invoked when the object is created
 		car.setTyres(4);
-    car.setType("Passenger");
+                car.setType("Passenger");
     
-    System.out.println("No of tyres: "+car.getTyres());
-    System.out.println("Vehicle Type: "+car.getType());
-    car.fuelType("Petrol/Diesel");
-    System.out.println();
-        
-    //truck
-    System.out.println("^^^^ TRUCK ^^^^");
+                System.out.println("No of tyres: "+car.getTyres());
+                System.out.println("Vehicle Type: "+car.getType());
+                car.fuelType("Petrol/Diesel");
+                System.out.println();
+		
+		//truck
+                System.out.println("^^^^ TRUCK ^^^^");
 		Vehicle truck  = new Vehicle(6,"Goods");
 		System.out.println("No of tyres: "+truck.getTyres());
-    System.out.println("Vehicle Type: "+truck.getType());
-    truck.fuelType("Diesel");
+                System.out.println("Vehicle Type: "+truck.getType());
+                truck.fuelType("Diesel");
 	}
 
 }
@@ -41,36 +41,37 @@ class Vehicle
 		this.tyres=2;
 		this.type="Passenger";
 		System.out.println("No of tyres: "+tyres);
-    System.out.println("Vehicle Type: "+type);
+                System.out.println("Vehicle Type: "+type);
 	}
 	//Parameterized Constructor
-    Vehicle(int tyres, String type){
-    this.tyres=tyres;
-    this.type=type;
-     }
+        Vehicle(int tyres, String type)
+	{
+		this.tyres=tyres;
+		this.type=type;
+	}
     
-    //Methods
-    public void fuelType(String ftype)
-    {
+        //Methods
+        public void fuelType(String ftype)
+        {
     	System.out.println("Fuel Type: "+ftype);
-    }
+        }
 	//Getters
 	public int getTyres()
 	{
 		return this.tyres;
 	}
-
 	public String getType()
 	{
 		return this.type;
 	}
 	//Setters
-	public void setTyres(int tyres){
-        this.tyres=tyres;
-    }
-
-  public void setType(String type){
-        this.type=type;
-    }
+	public void setTyres(int tyres)
+	{
+		this.tyres=tyres;
+	}
+        public void setType(String type)
+	{
+		this.type=type;
+	}
     
 }

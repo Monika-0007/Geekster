b1b45be4896a4d40b504cd976343cbc03d222d85
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-12-05 12:33:39 +0530

    Create test.java

---
+++ Assignments/Dec04/test.java
@@ -0,0 +1,19 @@
+package objects;
+
+public class Test1
+{
+    public static void talk()
+    {
+    	System.out.println("hello there!");
+    }
+    //Function talk is overloaded
+    public static void talk(String name)
+    {
+    	System.out.println("Hello "+name+"!");
+    }
+    public static void main(String[] args)
+    {
+    	talk("Matthew");
+    }
+}
+

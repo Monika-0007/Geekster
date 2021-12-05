8e18eacf82b3ebc9b110de27071544ebef52a687
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-12-05 11:50:48 +0530

    Update book.java

--- Assignments/Dec03/book.java
+++ Assignments/Dec03/book.java
@@ -1,7 +1,7 @@
 public class Main
 {
 	public static void main(String[] args)
-  {
+	{
 		MyBook new_novel=new MyBook(); 
 		new_novel.setTitle("A tale of two cities");
 		System.out.println("The title is: "+new_novel.getTitle());

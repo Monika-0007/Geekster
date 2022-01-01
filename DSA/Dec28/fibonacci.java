3feccf4a61bbbd2a75358b529d320f316db4db70
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2022-01-01 20:47:54 +0530

    Create fibonacci.java

---
+++ DSA/Dec28/fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class practice {
+    public static int fibonacci(int n)
+    {
+      if(n <=1)
+    	  return n;
+      else
+    	  return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+    public static void main(String args[])
+    {
+    	Scanner sc  = new Scanner(System.in);
+    	int n=sc.nextInt();
+    	for(int i = 1;i<=n;i++) {
+    	   System.out.print(fibonacci(i)+" ");        
+    	   }
+    }
+}

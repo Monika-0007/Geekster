24f0b9f55b932f6770b34b8a2a24df2309af0997
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2022-01-01 20:21:56 +0530

    Create wave.java

---
+++ DSA/Dec27/wave.java
@@ -0,0 +1,52 @@
+public class practice {
+	public static void main(String[] args) 
+	{
+		Scanner sc  = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int arr[][]=new int[a][b];
+		for(int i=0;i<a;i++)
+		{
+			for(int j=0;j<b;j++)
+			{
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("WAVE MATRIX(Horizontal Traverse)");
+		for(int i=0;i<b;i++)
+		{
+			if(i%2==0)
+			{
+				for(int j=0;j<a;j++)
+				{
+					System.out.print(arr[i][j]+" ");
+				}
+			}
+			else
+			{
+				for(int j=a-1;j>=0;j--)
+				{
+					System.out.print(arr[i][j]+" ");
+				}
+			}		
+		}
+		System.out.println("\nWAVE MATRIX(Vertical Traverse)");
+		for(int i=0;i<a;i++)
+		{
+			if(i%2==0)
+			{
+				for(int j=0;j<a;j++)
+				{
+					System.out.print(arr[j][i]+" ");
+				}
+			}
+			else
+			{
+				for(int j=a-1;j>=0;j--)
+				{
+					System.out.print(arr[j][i]+" ");
+				}
+			}		
+		}
+	}
+}

792e70137d9f6a4198617dbb4f13f2f7a412f080
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2022-01-02 15:31:50 +0530

    Create gcd.java

---
+++ DSA/Dec30/gcd.java
@@ -0,0 +1,13 @@
+class practice
+{
+	public static int gcd(int x,int y)
+	{
+		if(y==0)
+			return x;
+		else
+			return gcd(y,x%y);
+	}
+	public static void main(String[] args) {
+	System.out.println(gcd(4,8));
+	}
+}
